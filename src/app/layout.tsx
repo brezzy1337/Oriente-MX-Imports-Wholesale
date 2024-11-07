0cdca349cc5e10e7216f3dd0c1ab74cce206369c
Devin Phat <90067281+brezzy1337@users.noreply.github.com>
2024-11-07 17:22:55 +0000

    Added dynamic importing.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
-import type { Metadata } from 'next'                                                         
+import type { Metadata } from 'next'       
+import Head from 'next/head'
+import dynamic from 'next/dynamic'                                                  
  import { Inter } from 'next/font/google'                                                     
  import './globals.css'                                                                       
- import Navigation from '../components/Navigation'                                             
                                                                                               
  const inter = Inter({ subsets: ['latin'] })                                                  
                                                                                               
@@ -9,6 +10,11 @@ import type { Metadata } from 'next'
    title: 'Deli Asya - Thai Food Wholesale',                                                  
    description: 'Thai food and goods importer, wholesaler, and distributor',                  
  }                                                                                            
+
+ const Navigation = dynamic(() => import('@/components/Navigation'), {
+  ssr: false,
+})
+
                                                                                               
  export default function RootLayout({                                                         
    children,                                                                                  
@@ -16,10 +22,18 @@ import type { Metadata } from 'next'
    children: React.ReactNode                                                                  
  }) {                                                                                         
    return (                                                                                   
-     <html lang="en">                                                                         
+     <html lang="en">     
+     <Head>
+      <title>Deli Asiya</title>
+     </Head>                                                                    
        <body className={inter.className}>                                                     
-         <Navigation />                                                                       
+         <Navigation />                                    
+         <main>
          {children}                                                                           
+         </main>
+         <footer>
+          <p>@ 2024 Delu Asiya. All rights reserved</p>
+          </footer>                                   
        </body>                                                                                
      </html>                                                                                  
    )                                                                                          
