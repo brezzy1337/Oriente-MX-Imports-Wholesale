2222d526a23c0102c15a767bfff6f406d3d62a1e
Devin Phat <90067281+brezzy1337@users.noreply.github.com>
2024-11-07 10:02:48 +0000

    Ill just go pri

--- deli_asya/src/app/components/Navigation.tsx
+++ deli_asya/src/app/components/Navigation.tsx
@@ -1,4 +1,4 @@
-import Link from 'next/link'
+import Link from "next/link";
 
 const Navigation = () => {
   return (
@@ -8,30 +8,62 @@ const Navigation = () => {
           <div className="flex space-x-7">
             <div>
               <Link href="/" className="flex items-center py-4">
-                <span className="font-semibold text-gray-500 text-lg">Deli Asya</span>
+                <span className="font-semibold text-gray-500 text-lg">
+                  Deli Asya
+                </span>
               </Link>
             </div>
             <div className="hidden md:flex items-center space-x-1">
-              <Link href="/" className="py-4 px-2 text-gray-500                              
-hover:text-gray-900">Home</Link>
-              <Link href="/about" className="py-4 px-2 text-gray-500                         
-hover:text-gray-900">About</Link>
-              <Link href="/catalog" className="py-4 px-2 text-gray-500                       
-hover:text-gray-900">Catalog</Link>
-              <Link href="/contact" className="py-4 px-2 text-gray-500                       
-hover:text-gray-900">Contact</Link>
+              <Link
+                href="/"
+                className="py-4 px-2 text-gray-500                              
+hover:text-gray-900"
+              >
+                Home
+              </Link>
+              <Link
+                href="/about"
+                className="py-4 px-2 text-gray-500                         
+hover:text-gray-900"
+              >
+                About
+              </Link>
+              <Link
+                href="/catalog"
+                className="py-4 px-2 text-gray-500                       
+hover:text-gray-900"
+              >
+                Catalog
+              </Link>
+              <Link
+                href="/contact"
+                className="py-4 px-2 text-gray-500                       
+hover:text-gray-900"
+              >
+                Contact
+              </Link>
             </div>
           </div>
           <div className="hidden md:flex items-center space-x-3">
-            <Link href="/login" className="py-2 px-2 font-medium text-gray-500               
-hover:text-gray-900">Login</Link>
-            <Link href="/signup" className="py-2 px-2 font-medium text-white bg-green-500    
-rounded hover:bg-green-400">Sign Up</Link>
+            <Link
+              href="/login"
+              className="py-2 px-2 font-medium text-gray-500               
+hover:text-gray-900"
+            >
+              Login
+            </Link>
+            <Link
+              href="/signup"
+              className="py-2 px-2 font-medium text-white bg-green-500    
+rounded hover:bg-green-400"
+            >
+              Sign Up
+            </Link>
           </div>
         </div>
       </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navigation  
+export default Navigation;
