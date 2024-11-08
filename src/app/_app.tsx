471f424fadb0773328bbca68dea60fd6b237e702
Devin Phat <90067281+brezzy1337@users.noreply.github.com>
2024-11-08 00:40:16 +0000

    Added _app.tsx which should passes the styles and props

---
+++ src/app/_app.tsx
@@ -0,0 +1,23 @@
+import type { ReactElement, ReactNode } from 'react'
+import type { NextPage } from 'next'
+import type { AppProps } from 'next/app'
+import RootLayout from './layout'
+import '@/src/styles/globals.css'
+
+export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
+  getLayout?: (page: ReactElement) => ReactNode
+}
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout
+}
+
+function App({ Component, pageProps: { session, ...pageProps } }: AppPropsWithLayout) {
+  return (
+        <RootLayout>
+          <Component {...pageProps} />
+        </RootLayout>
+  )
+}
+
+export default App
