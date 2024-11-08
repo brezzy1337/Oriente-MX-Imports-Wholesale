5a816febba8ed7fbbae26da21fecd130907fe7b7
Devin Phat <90067281+brezzy1337@users.noreply.github.com>
2024-11-08 00:33:32 +0000

    styles troubleshoointg

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,40 +1,37 @@
-import type { Metadata } from 'next'       
-import Head from 'next/head'
-import dynamic from 'next/dynamic'                                                  
- import { Inter } from 'next/font/google'                                                     
- import '@/src/styles/globals.css'                                                                       
-                                                                                              
- const inter = Inter({ subsets: ['latin'] })                                                  
-                                                                                              
- export const metadata: Metadata = {                                                          
-   title: 'Deli Asya - Thai Food Wholesale',                                                  
-   description: 'Thai food and goods importer, wholesaler, and distributor',                  
- }                                                                                            
+import type { Metadata } from "next";
+import Head from "next/head";
+import dynamic from "next/dynamic";
+import { Inter } from "next/font/google";
+import "@/src/styles/globals.css";
 
- const Navigation = dynamic(() => import('@/components/Navigation'), {
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Deli Asya - Thai Food Wholesale",
+  description: "Thai food and goods importer, wholesaler, and distributor",
+};
+
+const Navigation = dynamic(() => import("@/components/Navigation"), {
   ssr: false,
-})
+});
 
-                                                                                              
- export default function RootLayout({                                                         
-   children,                                                                                  
- }: {                                                                                         
-   children: React.ReactNode                                                                  
- }) {                                                                                         
-   return (                                                                                   
-     <html lang="en">     
-     <Head>
-      <title>Deli Asiya</title>
-     </Head>                                                                    
-       <body className={inter.className}>                                                     
-         <Navigation />                                    
-         <main>
-         {children}                                                                           
-         </main>
-         <footer>
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <Head>
+        <title>Deli Asiya</title>
+      </Head>
+      <body className={inter.className}>
+        <Navigation />
+        <main>{children}</main>
+        <footer>
           <p>@ 2024 Delu Asiya. All rights reserved</p>
-          </footer>                                   
-       </body>                                                                                
-     </html>                                                                                  
-   )                                                                                          
- }             
+        </footer>
+      </body>
+    </html>
+  );
+}
