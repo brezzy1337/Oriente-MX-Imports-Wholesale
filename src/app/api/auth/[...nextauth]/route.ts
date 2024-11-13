c301f42a9a2a12ca412ba8a7b6ce52226a9ef28e
Devin Phat <90067281+brezzy1337@users.noreply.github.com>
2024-11-13 05:42:53 +0000

    Next-auth with app router requires a different format for the clinet side API handler.

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,4 +1,5 @@
-import NextAuth from "next-auth"                                                          
-import { authOptions } from "@/server/auth"
-                                                                                          
-export default NextAuth(authOptions) 
+import NextAuth from "next-auth/next";
+import { authOptions } from "@/server/auth";
+
+const handler = NextAuth(authOptions);
+export { handler as GET, handler as POST };
