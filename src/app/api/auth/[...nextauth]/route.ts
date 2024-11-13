803cdb2c50cfc93aa19d9e2571a19eb31a0fb3df
Devin Phat <90067281+brezzy1337@users.noreply.github.com>
2024-11-13 07:23:31 +0000

    Changed type to fix type issue.

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,5 @@
 import NextAuth from "next-auth/next";
 import { authOptions } from "@/server/auth";
 
-const handler = NextAuth(authOptions) as unknown;
+const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST };
