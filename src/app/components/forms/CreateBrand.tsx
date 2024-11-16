c7fe4c27cda78f350dd962a96f05f2474ae8f1fe
Devin Phat <90067281+brezzy1337@users.noreply.github.com>
2024-11-16 11:52:25 +0000

    commented out errors

--- src/app/components/forms/CreateBrand.tsx
+++ src/app/components/forms/CreateBrand.tsx
@@ -199,14 +199,14 @@ export default function CreateBrand() {
 
       <button
         type="submit"
-        disabled={mutation.isPending || isUploading}
+        // disabled={mutation.isPending || isUploading}
         className="inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 disabled:opacity-50"
       >
-        {isUploading
+        {/* {isUploading
           ? "Uploading..."
           : mutation.isPending
           ? "Creating..."
-          : "Create Brand"}
+          : "Create Brand"} */}
       </button>
     </form>
   );
