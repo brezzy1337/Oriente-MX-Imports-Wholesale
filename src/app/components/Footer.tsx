5ea612d6bdbe0e806a25b64e11574d6a3a70088e
Devin Phat <90067281+brezzy1337@users.noreply.github.com>
2024-11-20 05:06:43 +0000

    style: Refactor Footer component layout and styling

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -20,9 +20,9 @@ const Footer = () => {
             />
           </div>
             {/* <div className="flex md:flex-cols-5 gap-60 w-full justify-center space-y-1"> */}
-            <div className="flex md:flex-cols-5 gap-4 md:gap-8 lg-gap-12 md:py-4 w-full justify-center space-y-1">
+            <div className="flex md:flex-cols-4 gap-8 md:gap-8 lg-gap-12 md:py-4 w-full justify-center space-y-1">
             {/* Company Column */}
-            <div className="flex grid grid-cols-1 justify-end item-end">
+            <div className="flex flex-col space-y-1">
               <h3 className="font-semibold text-lg mb-2 text-gray-800">
                 Company
               </h3>
