5704aa7e6631ba120bc21f1b3d10f4bc2bac1376
Devin Phat <90067281+brezzy1337@users.noreply.github.com>
2024-11-22 23:29:25 +0000

    Removed text from nostros image

--- src/app/nosotros/page.tsx
+++ src/app/nosotros/page.tsx
@@ -13,9 +13,9 @@ export default function AboutUs() {
           priority
         />
         <div className="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center">
-          <h1 className="text-4xl md:text-5xl text-primary font-bold">
+          {/* <h1 className="text-4xl md:text-5xl text-primary font-bold">
             Nosotros
-          </h1>
+          </h1> */}
         </div>
       </div>
 
