23c806ce27d07c8f4435a88aea28b62a43dcecb6
Devin Phat <90067281+brezzy1337@users.noreply.github.com>
2024-11-23 02:38:07 +0000

    Created FeaturedBrands component for better rednering

---
+++ src/app/components/FeaturedBrands.tsx
@@ -0,0 +1,51 @@
+'use client'
+
+import { useState, useEffect } from "react";
+import Image from "next/image";
+import DecoratedHeader from "./DecoratedHeader"
+import { getBrands } from "../functions/_serverActions"
+import { getBlobUrl } from "@/utils/blob";
+
+const FeaturedBrands = () => {
+
+    const [brands, setBrands] = useState<any[]>([]);
+
+
+    const fetchBrands = async () => {
+        const result = await getBrands();
+        if (result.success && result.data) {
+            // setIsLoading(false);
+            setBrands(result.data);
+        }
+        return result.data;
+    };
+
+    useEffect(() => {
+        fetchBrands();
+    }, []);
+
+    return (
+        <div className="container mx-auto px-4">
+            <DecoratedHeader>
+                Nuestras Marcas
+            </DecoratedHeader>
+            <div className="grid grid-cols-2 md:grid-cols-5 gap-8 max-w-4xl mx-auto">
+                {brands.map((brand) => (
+                    <a href={`/comercio/brands/${brand.slug}`} key={brand.id} className="group">
+                        <div className="bg-white p-4 hover:shadow-md transition-shadow">
+                            <Image
+                                src={getBlobUrl(brand.logoUrl)}
+                                alt={brand.name}
+                                width={200}
+                                height={200}
+                                className="w-full h-auto"
+                            />
+                        </div>
+                    </a>
+                ))}
+            </div>
+        </div>
+    )
+};
+
+export default FeaturedBrands;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,8 @@
 "use client";
 
 import dynamic from 'next/dynamic'
-import Image from "next/image";
-// import HeroGallery from "@/app/components/HeroGallery";
-// import ServiceCards from "@/app/components/ServiceCards";
-// import FeaturedProducts from "@/app/components/FeaturedProducts";
-import DecoratedHeader from "@/app/components/DecoratedHeader";
-import { useState, useEffect } from "react"
-import { getBrands } from "./functions/_serverActions";
-
-import { getBlobUrl } from "@/utils/blob";
 
 export default function Home() {
-
-  const [brands, setBrands] = useState<any[]>([]);
-  // const [isLoading, setIsLoading] = useState(true);
-
-  const fetchBrands = async () => {
-    const result = await getBrands();
-    if (result.success && result.data) {
-      // setIsLoading(false);
-      setBrands(result.data);
-    }
-    return result.data;
-  };
-
-  useEffect(() => {
-    fetchBrands();
-  }, []);
 
   const HeroGallery = dynamic(() => import("@/app/components/HeroGallery"), {
     ssr: false,
@@ -44,6 +19,11 @@ export default function Home() {
     // loading: () => <div>Loading...</div>
   });
 
+  const FeaturedBrands = dynamic(() => import("@/app/components/FeaturedBrands"), {
+    ssr: false,
+    // loading: () => <div>Loading...</div>
+  });
+
   return (
     <main className="flex min-h-screen flex-col">
       <HeroGallery />
@@ -51,27 +31,8 @@ export default function Home() {
       <section className="container mx-auto px-4 py-12">
         <ServiceCards />
       </section>
+      <FeaturedBrands />
       <section className="bg-gray-50">
-        <div className="container mx-auto px-4">
-          <DecoratedHeader>
-          Nuestras Marcas
-          </DecoratedHeader>
-          <div className="grid grid-cols-2 md:grid-cols-5 gap-8 max-w-4xl mx-auto">
-            {brands.map((brand) => (
-              <a href={`/comercio/brands/${brand.slug}`} key={brand.id} className="group">
-                <div className="bg-white p-4 hover:shadow-md transition-shadow">
-                  <Image
-                    src={getBlobUrl(brand.logoUrl)}
-                    alt={brand.name}
-                    width={200}
-                    height={200}
-                    className="w-full h-auto"
-                  />
-                </div>
-              </a>
-            ))}
-          </div>
-        </div>
       </section>
     </main>
   );
